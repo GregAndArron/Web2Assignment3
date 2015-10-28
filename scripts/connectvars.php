010dc8ec23d0d6f899b1bfeb856d742e1a59cb75
Arron Dick <s_drac2@yahoo.com>
2015-10-29 11:59:56 +1300

    test commit
    Again not working

--- scripts/connectvars.php
+++ scripts/connectvars.php
@@ -9,6 +9,8 @@ Date: 24/10/2015 - 4:49:47 PM
 
 //Define database connection constants
 //This is the file for usage on laptop
+
+//Another test
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_USER', 'USERNAME');
