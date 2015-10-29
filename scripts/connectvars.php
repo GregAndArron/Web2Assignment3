9ed2187644486fca9b39adff736fac5e397c484e
doglitbug <dickaj1@student.op.ac.nz>
2015-10-29 15:12:02 +1300

    Another test
    Not working again

--- scripts/connectvars.php
+++ scripts/connectvars.php
@@ -11,6 +11,7 @@ Date: 24/10/2015 - 4:49:47 PM
 //This is the file for usage on laptop
 
 //Another test
+//testing using .git/info/exclude.
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_USER', 'USERNAME');
