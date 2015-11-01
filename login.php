9dca98e6808952945cc79c2e4257aa54d989ef0e
Arron Dick <s_drac2@yahoo.com>
2015-11-01 21:40:32 +1300

    Rewrote login.php so there is only one fail message rather than several, as with security surround logins, we only report that username/password incorrect for any failure to log in

--- login.php
+++ login.php
@@ -1,13 +1,16 @@
 <?php
-
 //Start the session
 session_start();
+
 //Set up vars for database connection
 include "scripts/connectvars.php";
 
+//Varible to indicate sucessful login
+$success = false;
+
 //Check required form data is present and has content
 if (!empty($_POST['username']) && !empty($_POST['password'])) {
-    //Connect to database
+//Connect to database
     $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Cant connect to database: ' . mysqli_error($dbc));
 
     //Grab the user-entered log-in data and sanitize
@@ -24,36 +27,35 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
     //Close connection as we are done with it
     mysqli_close($dbc);
 
-    //Check results for query
-    //Checks username exists
+    //Check results of query
+    //Checks there is one record of the username
     if (mysqli_num_rows($result) == 1) {
         //Grab data from query
         $row = mysqli_fetch_assoc($result);
 
-        // Hashing the password with its hash as the salt returns the same hash
-        //Checks password is correcr
+        //Hashing the password with its hash as the salt returns the same hash(symetrical?)
+        //Checks password is correct
         if (crypt($password, $row['password']) === $row['password']) {
             //We have a matching user/password combo in the database so grab the data and set session vars
-            //Stuff retrieved data into $_SESSION
-            //This depends on the data returned from the query...
+            //This depends on the data requested in the select query!
             foreach ($row as $key => $value) {
-                //Don't count password field!
+                //Ignore the password field!
                 if ($key != "password") {
                     $_SESSION[$key] = $value;
                 }
             }
-            //Redirect to landing page
-            echo "You have successfully logged in<br/><a href='home.php'>Click here to return home</a>";
+            $success = true;
         }
-        //Password incorrect
-        echo "Username and/or password incorrect<br/><a href='home.php'>Click here to return home</a>";
-    } else {
-        //Username incorrect
-        echo "Username and/or password incorrect<br/><a href='home.php'>Click here to return home</a>";
     }
-} else {
-    //Username and/or password not supplied, this should happen due to checking on form!
-    //This will trip if page is directly visited!!
-    echo "Username and/or password required<br/><a href='home.php'>Click here to return home</a>";
 }
+
+if ($success == true) {
+    echo "You have successfully logged in<br/><a href='home.php'>Click here to return home</a>";
+} else {
+    echo "Username and/or password incorrect<br/><a href='home.php'>Click here to return home</a>";
+}
+//TODO: Insert footer?
 ?>
+
+</body>
+</html>
