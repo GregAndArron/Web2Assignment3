7921d336f12f94ae82f68bcd006bd2002d256451
Arron Dick <s_drac2@yahoo.com>
2015-11-06 12:11:13 +1300

    Validation on dataEntry Adding items to database complete

--- dataEntry.php
+++ dataEntry.php
@@ -11,10 +11,78 @@ require_once("scripts/navbar.php");
             <h1>Data Entry</h1>
         </div>
         <div class="middleTop col-sm-6">
+            <?php
+            //Before starting the form, get previous if posted
+            //Used for sticky data
+            $category = $price = $comment = $date = "";
+            //Used for errors, comment not included here
+            $categoryError = $priceError = $dateError = "";
+            //TODO: data validation
+            if (isset($_POST['submit'])) {
+                //Get submitted data and sanitize
+                //Category
+                if (isset($_POST['category'])) {
+                    $category = mysqli_escape_string($dbc, trim($_POST['category']));
+                    //Check category_id is within the category table
+                    $checkQuery = "SELECT * FROM tbl_category WHERE category_id='$category'";
+                    $result = mysqli_query($dbc, $checkQuery) or die("Couldn't get list of categories: ") . mysqli_error($dbc);
+                    if (mysqli_num_rows($result) != 1) {
+                        $categoryError = "Please select a valid category.";
+                    }
+                } else {
+                    $categoryError = "Category is required.";
+                }
+
+                //Price
+                if (isset($_POST['price'])) {
+                    $price = mysqli_escape_string($dbc, trim($_POST['price']));
+
+                    //Check price is a valid number
+                    if ((float) ($price / 0.01) != (int) ($price / 0.01) || $price == "") {
+                        $price = "";
+                        $priceError = "Please reenter price.";
+                    }
+                } else {
+                    $priceError = "Price is required.";
+                }
+
+                //Comment
+                if (isset($_POST['comment'])) {
+                    $comment = mysqli_escape_string($dbc, trim($_POST['comment']));
+                }
+
+                //Date
+                if (isset($_POST['date'])) {
+                    //TODO Check valid date
+                    //According to Dale, this is not required to be checked if we are using a date picker on our form, even in the real world...
+                    //TODO: Use todays date if not provided or default to that date on form?
+                    $date = mysqli_escape_string($dbc, trim($_POST['date']));
+                    if ($date=="") {
+                        $dateError = "Date is required.";
+                    }
+                } else {
+                    $dateError = "Date is required.";
+                }
+                //Check all required data is provided and valid(ie no errors have been made)
+                if ($categoryError . $priceError . $dateError == "") {
+                    $insertQuery = "INSERT INTO tbl_item (user_id,category_id,amount,comment,date)"
+                            . " VALUES ('{$_SESSION['user_id']}','$category','$price','$comment','$date')";
+
+                    mysqli_query($dbc, $insertQuery) or die("Couldn't add item to database " . mysqli_error($dbc));
+                    //Clear out previous sticky data as it is now been added to database
+                    $category = $price = $comment = $date = "";
+                }
+            }//End (isset($_POST['submit']))
+            //Enter a default date
+            if ($date==""){
+                $date=date('Y-m-d');
+            }
+            ?>
             <form id="myForm" action="#" method="POST">
                 <div id="item">
                     <label>Category</label>
                     <select name="category">
+                        <option>Select...</option>
                         <?php
                         //Get categories
                         $query = "SELECT * from tbl_category ORDER BY description ASC";
@@ -22,23 +90,33 @@ require_once("scripts/navbar.php");
                         $result = mysqli_query($dbc, $query) or die("Couldn't get list of categories: " . mysqli_error($dbc));
 
                         while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<option value='{$row['category_id']}'>{$row['description']}</option>";
+                            echo "<option value='{$row['category_id']}'";
+                            //Sticky data
+                            if ($row['category_id'] == $category) {
+                                echo " selected ";
+                            }
+                            echo ">{$row['description']}</option>";
                         }
                         ?>
                     </select>
 
                     <label>Price</label>
-                    <input type="number" step="0.01" name="price"/>
+                    <input type="number" step="0.01" name="price" value="<?php echo $price; ?>"/>
 
                     <label>Comment/place</label>
-                    <input type="text" width="20" name="comment"/>
+                    <input type="text" width="20" name="comment" value="<?php echo $comment; ?>"/>
 
                     <label >Date</label>
-                    <input type="date" name="date"/>
+                    <input type="date" name="date" value="<?php echo $date; ?>"/>
 
                     <input type="submit" value="Add item" name="submit"/>
                 </div>
             </form>
+            <?php
+            //Print out errors
+            //TODO: put it on the actaul form...
+            echo $categoryError." ".$priceError ." ".$dateError ;
+            ?>
         </div>
         <h2>Todays items</h2>
         <div class="middleBottom col-sm-6">
@@ -53,7 +131,7 @@ require_once("scripts/navbar.php");
 
                 $result = mysqli_query($dbc, $query) or die("Couldn't get user data; " . mysqli_error($dbc));
 
-                //Show results in a pretty table
+//Show results in a pretty table
                 if (mysqli_num_rows($result) > 0) {
                     //Table header
                     echo "<table class='table table-striped'><tr><th>Category</th><th>Amount</th><th>Date</th><th>Comment</th></tr>";
