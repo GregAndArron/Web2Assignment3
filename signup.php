48234350a95c10510af38e29dba7f6a080111944
Arron Dick <s_drac2@yahoo.com>
2015-11-09 20:44:58 +1300

    Validation of username and email

--- signup.php
+++ signup.php
@@ -35,6 +35,7 @@ function encrypt($password) {
             $secret_word = $username = $fullName = $email = $fullName = "";
             //Set up varibles for error messages
             $secret_wordError = $usernameError = $fullNameError = $emailError = $passwordError = $password_confirmError = "";
+
             //Check for previously sumbitted data
             if (isset($_POST['submit'])) {
                 //Check secret word
@@ -42,7 +43,7 @@ function encrypt($password) {
                     //Sanitize secret word from form and strip leading/trailing spaces
                     $secret_word = mysqli_escape_string($dbc, trim($_POST['secret_word']));
 
-                    //check the secret word is correct
+                    //Check the secret word is correct
                     if ($secret_word != "BITRulez") {
                         $secret_wordError = "Registration word is incorrect";
                     }
@@ -52,27 +53,56 @@ function encrypt($password) {
                 if (isset($_POST['username'])) {
                     //Sanitize username from form and strip leading/trailing spaces
                     $username = mysqli_escape_string($dbc, trim($_POST['username']));
-                    //TODO Check validity with regex
-
-                    $selectQuery = "SELECT username FROM `tbl_user` WHERE username='$username'";
-                    $result = mysqli_query($dbc, $selectQuery) or die("Couldn't check user is not in database already: " . mysqli_error($dbc));
-                    if (mysqli_num_rows($result) != 0) {
-                        $usernameError = "Username is already taken, please choose another.";
+                    //CHeck username isn't empty after trimming spaces
+                    if ($username != "") {
+                        //Check validity with regex(letters, numbers and _)
+                        if (!preg_match("/([^\w]+)/i", $username)) {
+                            //Check username is not already in the database
+                            $selectQuery = "SELECT username FROM `tbl_user` WHERE username='$username'";
+                            $result = mysqli_query($dbc, $selectQuery) or die("Couldn't check username is not in database already: " . mysqli_error($dbc));
+                            if (mysqli_num_rows($result) != 0) {
+                                $usernameError = "Username is already taken, please choose another";
+                            }
+                        } else {
+                            $usernameError = "Please enter a username using only letters, numbers or the underscore";
+                        }
                     } else {
-                        //User name is avaliable!
+                        $usernameError = "Please enter a username";
                     }
                 } else {
                     $usernameError = "Please enter a username";
                 }
 
-                //TODO Check fullName
-               $fullName="Full name matey";
-               
-                //TODO Check email is valid and not already used?
+                //Check fullName
+                if (isset($_POST['fullName'])) {
+                    $fullName = mysqli_escape_string($dbc, trim($_POST['fullName']));
+                    //Check there is a name entered
+                    if (!empty($fullName)) {
+                        
+                    } else {
+                        $fullNameError = "Please enter your name";
+                    }
+                } else {
+                    $fullNameError = "Please enter your name";
+                }
+
+                //Check email is valid and not already used?
                 if (isset($_POST['email'])) {
                     //Sanitize username from form and strip leading/trailing spaces
                     $email = mysqli_escape_string($dbc, trim($_POST['email']));
-                    //TODO Check validity with regex
+                    //Check email is of a valid format, using PHPs inbuilt functions, applies RFC 822
+                    //Regex, what regex? D.R.Y. ...
+
+                    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        //Check email has not already been used
+                        $selectQuery = "SELECT email FROM `tbl_user` WHERE email='$email'";
+                        $result = mysqli_query($dbc, $selectQuery) or die("Couldn't check database for email address: " . mysqli_error($dbc));
+                        if (mysqli_num_rows($result) != 0) {
+                            $emailError = "Email address has already been used, please try another";
+                        }
+                    } else {
+                        $emailError = "Please enter an email address in the format \"name@provider.domain\"";
+                    }
                 } else {
                     $emailError = "Please enter an email address";
                 }
@@ -84,7 +114,7 @@ function encrypt($password) {
                             . "VALUES ('$username','$fullName','$email','" . encrypt($password) . "')";
 
                     debug($insertQuery);
-                    
+
                     $result = mysqli_query($dbc, $insertQuery) or die("Couldn't add new user to the system: " . mysqli_error($dbc));
 
                     //Send them to the log in page
@@ -98,7 +128,7 @@ function encrypt($password) {
                     <div id = "legend">
                         <legend class = "">Register</legend>
                     </div>
-                    
+
                     <div class = "control-group">
                         <label class = "control-label" for = "username">Username</label>
                         <div class = "controls">
@@ -107,50 +137,50 @@ function encrypt($password) {
                             <span class="error"><?php echo $usernameError; ?></span>
                         </div>
                     </div>
-					<div class="control-group">
-						<label class="control-label"  for="fullName">Full name</label>
-						<div class="controls">
-							<input type="text" id="fullName" name="fullName" placeholder="" class="input-xlarge input-mysize">
-							<p class="help-block">Your full Name can contain any letters, with spaces</p>
-						</div>
-                    <div class = "control-group">
-                        <label class = "control-label" for = "email">E-mail</label>
-                        <div class = "controls">
-                            <input type = "text" id = "email" name = "email" placeholder = "" class = "input-xlarge input-mysize" required value="<?php echo $email; ?>">
-                            <p class = "help-block">Please provide your E-mail</p>
-                            <span class="error"><?php echo $emailError; ?></span>
+                    <div class="control-group">
+                        <label class="control-label"  for="fullName">Full name</label>
+                        <div class="controls">
+                            <input type="text" id="fullName" name="fullName" placeholder="" class="input-xlarge input-mysize" required value="<?php echo $fullName; ?>">
+                            <p class="help-block">Your full Name can contain any letters, with spaces</p>
                         </div>
-                    </div>
+                        <div class = "control-group">
+                            <label class = "control-label" for = "email">E-mail</label>
+                            <div class = "controls">
+                                <input type = "text" id = "email" name = "email" placeholder = "" class = "input-xlarge input-mysize" required value="<?php echo $email; ?>">
+                                <p class = "help-block">Please provide your E-mail</p>
+                                <span class="error"><?php echo $emailError; ?></span>
+                            </div>
+                        </div>
 
-                    <div class = "control-group">
-                        <label class = "control-label" for = "password">Password</label>
-                        <div class = "controls">
-                            <input type = "password" id = "password" name = "password" placeholder = "" class = "input-xlarge input-mysize" required>
-                            <p class = "help-block">Password should be at least 8 characters</p>
-                            <span class="error"><?php echo $passwordError; ?></span>
+                        <div class = "control-group">
+                            <label class = "control-label" for = "password">Password</label>
+                            <div class = "controls">
+                                <input type = "password" id = "password" name = "password" placeholder = "" class = "input-xlarge input-mysize" required>
+                                <p class = "help-block">Password should be at least 8 characters</p>
+                                <span class="error"><?php echo $passwordError; ?></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class = "control-group">
-                        <label class = "control-label" for = "password_confirm">Password (Confirm)</label>
-                        <div class = "controls">
-                            <input type = "password" id = "password_confirm" name = "password_confirm" placeholder = "" class = "input-xlarge input-mysize" required>
-                            <p class = "help-block">Please confirm password</p>
-                            <span class="error"><?php echo $password_confirmError; ?></span>
+                        <div class = "control-group">
+                            <label class = "control-label" for = "password_confirm">Password (Confirm)</label>
+                            <div class = "controls">
+                                <input type = "password" id = "password_confirm" name = "password_confirm" placeholder = "" class = "input-xlarge input-mysize" required>
+                                <p class = "help-block">Please confirm password</p>
+                                <span class="error"><?php echo $password_confirmError; ?></span>
+                            </div>
                         </div>
-                    </div>
-					<div class = "control-group">
-                        <label class = "control-label" for = "secret_word">Registration word</label>
-                        <div class = "controls">
-                            <input type = "secret_word" id = "secret_word" name = "secret_word" placeholder = "" class = "input-xlarge input-mysize" required value="<?php echo $secret_word; ?>">
-                            <p class = "help-block">Please enter the registration word you were given</p>
-                            <span class="error"><?php echo $secret_wordError; ?></span>
+                        <div class = "control-group">
+                            <label class = "control-label" for = "secret_word">Registration word</label>
+                            <div class = "controls">
+                                <input type = "secret_word" id = "secret_word" name = "secret_word" placeholder = "" class = "input-xlarge input-mysize" required value="<?php echo $secret_word; ?>">
+                                <p class = "help-block">Please enter the registration word you were given</p>
+                                <span class="error"><?php echo $secret_wordError; ?></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class = "control-group">
-                        <div class = "controls">
-                            <input type="submit" value="Register" name="submit" class = "btn btn-success"/>
+                        <div class = "control-group">
+                            <div class = "controls">
+                                <input type="submit" value="Register" name="submit" class = "btn btn-success"/>
+                            </div>
                         </div>
-                    </div>
                 </fieldset>
             </form>
         </div>
